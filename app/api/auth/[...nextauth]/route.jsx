71efc0f165115bed729f8b27eebb2922160e1cc5
Aman_Purohit <amanpurohit785@gmail.com>
2023-12-27 12:26:31 +0530

    make my first next js backend api routing

---
+++ app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import connectedToDb from "@utils/db";
+import User from "@models/user";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {
+    // get the data of user every time to run the session
+    // making sure which user is currently online
+        const sessionUser=await User.findOne({
+            email:session.user.email
+        })
+
+        session.user.id=sessionUser._id.toString();
+
+        return session;
+
+  },
+  async signIn({ profile }) {
+    try {
+      await connectedToDb();
+
+      //check if a user exists or not
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image:profile.picture
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+
+      return false;
+    }
+  },
+});
+
+export {handler as GET, handler as POST};
---
+++ utils/db.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+let isDbConnected = false;
+
+const connectedToDb = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isDbConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "share-prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isDbConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectedToDb;
